62bb07f4530f76af1028fd941f610a1242f9124c
Stefan Eiermann <github@ultraapp.de>
2020-07-31 22:03:53 +0200

    Update demo.bash

--- demo.bash
+++ demo.bash
@@ -49,7 +49,7 @@ ARCHIVE_CHECKSUM=$(sha256sum ${TEMP_UPLOAD_DIR}/${ARCHIVE_NAME}.docarea | awk '{
 echo "Request permission"
 tokenrequest=$(curl -s \
   -X POST \
-  -d 'grant_type=client_credentials&scope=upload_documentation&client_id='$CLIENT_ID'&client_secret='$CLIENT_SECRET \
+  -d 'grant_type=client_credentials&scope=upload_documentation&client_id='${CLIENT_ID}'&client_secret='${CLIENT_SECRET} \
   ${API_ENDPOINT}/oauth2/token/)
 echo $tokenrequest
 token=$(echo $tokenrequest | jq -r '.access_token')
